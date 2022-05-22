6a36d779749f00a33c1898dac68349bdc2484455
imyip <imyip@imyip.net>
2022-05-22 19:34:04 +0800

    测试部署

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM node:16-alpine
 USER root
-RUN apk upgrade --available
-RUN apk add --no-cache -U jq socat && rm -rf /var/cache/apk/*
+RUN apk update
+RUN apk add --no-cache -U socat && rm -rf /var/cache/apk/*
 RUN npm config set user root \
 	&& npm config set registry https://registry.npm.taobao.org \
 	&& npm install -g joplin 
 RUN mkdir -p /joplin/profile 
-COPY runtime.sh /
+COPY runtime.sh /root/
 VOLUME /joplin/profile
 EXPOSE 41182
 CMD chown root:root /etc/crontabs/root && /usr/sbin/crond -f
-ENTRYPOINT ["/runtime.sh"]
+ENTRYPOINT ["/root/runtime.sh"]
--- runtime.sh
+++ runtime.sh
@@ -5,13 +5,8 @@ echo "*/30 * * * * /usr/local/bin/joplin sync" >/etc/crontabs/root
 
 socat TCP4-LISTEN:41182,fork TCP4:127.0.0.1:41184 &
 
-if [ -f '/joplin/options.json' ]; then
-   jq --raw-output '. | to_entries | .[] | "/usr/local/bin/joplin --profile /joplin/profile config " + .key + " \"" + .value + "\""' /joplin/options.json >/tmp/setoptions.sh
-   sh /tmp/setoptions.sh
-fi
-
-if [ -f '/joplin/profile/1.json']; then
-   /usr/local/bin/joplin config --import-file /joplin/profile/1.json
+if [ -f "/joplin/profile/options.json" ]; then
+   /usr/local/bin/joplin config --import-file /joplin/profile/options.json
    /usr/local/bin/joplin sync
 fi
 
