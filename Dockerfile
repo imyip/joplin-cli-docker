967ed0684b0b62f08570b82a022dc601103bd289
imyip <imyip@imyip.net>
2022-05-22 18:53:35 +0800

    test deploy

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,13 @@
 FROM node:16-alpine
+USER root
 RUN apk upgrade --available
-RUN apk add --no-cache -U dcron jq socat
+RUN apk add --no-cache -U jq socat && rm -rf /var/cache/apk/*
 RUN npm config set user root \
 	&& npm config set registry https://registry.npm.taobao.org \
 	&& npm install -g joplin 
-RUN mkdir -p /joplin/profile \
-	&& chown node:node /joplin/profile \
-	&& chown node:node /usr/sbin/crond 
+RUN mkdir -p /joplin/profile 
 COPY runtime.sh /
-USER node
 VOLUME /joplin/profile
 EXPOSE 41182
+CMD chown root:root /etc/crontabs/root && /usr/sbin/crond -f
 ENTRYPOINT ["/runtime.sh"]
--- runtime.sh
+++ runtime.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 set -e
-mkdir -p /home/node/crontabs
-touch /home/node/crontabs/node
-echo "*/30 * * * * /usr/local/bin/joplin sync" > /home/node/crontabs/node
-crond -b -l 8 -c /home/node/crontabs
+
+echo "*/30 * * * * /usr/local/bin/joplin sync" >/etc/crontabs/root
 
 socat TCP4-LISTEN:41182,fork TCP4:127.0.0.1:41184 &
 
 if [ -f '/joplin/options.json' ]; then
-   jq --raw-output '. | to_entries | .[] | "/usr/local/bin/joplin --profile /joplin/profile config " + .key + " \"" + .value + "\""' /joplin/options.json > /tmp/setoptions.sh
+   jq --raw-output '. | to_entries | .[] | "/usr/local/bin/joplin --profile /joplin/profile config " + .key + " \"" + .value + "\""' /joplin/options.json >/tmp/setoptions.sh
    sh /tmp/setoptions.sh
 fi
 
-/usr/local/bin/joplin --profile /joplin/profile $@
+if [ -f '/joplin/profile/1.json']; then
+   /usr/local/bin/joplin config --import-file /joplin/profile/1.json
+   /usr/local/bin/joplin sync
+fi
+
+/usr/local/bin/joplin --profile /joplin/profile &
 /usr/local/bin/joplin server start
